c4ca7615c6fbe4fc86333addc625683a91634d4d
Nathan Scott <contact@nathanscott.net>
2021-02-24 11:50:13 +0000

    added handleChange

--- src/counter/index.tsx
+++ src/counter/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { AppDispatch, RootState } from '../store';
 import { decrement, increment, reset } from './counterSlice';
@@ -7,29 +7,29 @@ import '../App.css';
 export function Counter() {
     const count = useSelector((state: RootState) => state.counter.value);
     const dispatch: AppDispatch = useDispatch(); // correct implementation of this? is there a need for an individual counter dispatch?
-    const mapDispatchToProps = (dispatch: any) => {
-        return {
-          // dispatching plain actions
-          increment: () => dispatch({ type: 'INCREMENT' }),
-          decrement: () => dispatch({ type: 'DECREMENT' }),
-          reset: () => dispatch({ type: 'RESET' })
-        }
+
+      const handleChange = (increment?: any, decrement?: any, reset?: any) => {
+        if(increment) return dispatch(increment)
+        else if(decrement) return dispatch(decrement)
+        else if(reset) return dispatch(reset)
+        else return;
       }
     return (
         <div className="counter-container">
+            Count: {count}
             <div className="counter">
                 <button 
                 aria-label="Increment value by 1"
-                onClick={()=> mapDispatchToProps(dispatch(increment()))}
+                onClick={() => handleChange(increment())}
                 >Increment</button>
                 <span>{count}</span>
                 <button 
                 aria-label="Decrement value by 1"
-                onClick={()=> dispatch(decrement())}
+                onClick={()=> handleChange(decrement())}
                 >Decrement</button>
                   <button 
                 aria-label="reset value to 0"
-                onClick={()=> mapDispatchToProps(dispatch(reset()))}
+                onClick={()=> handleChange(reset())}
                 >reset</button>
             </div>
         </div>
